932bc1935762ed5f135c7c304446eb918b8a45c5
KatKurowska <kata.kurowska@gmail.com>
2020-08-27 22:21:39 +0200

    test2

--- src/com/company/Blackjack.java
+++ src/com/company/Blackjack.java
@@ -1,8 +1,33 @@
 package com.company;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-public class Blackjack {
+
+//Application is an abstract class - all methods must be implemented
+
+
+public class Blackjack extends Application {
 
     public static void main (String[] args) {
-        //write your code here
+        launch(args);
+        // launch (args); - standard procedure to launch app with JavaFX
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 300,300, Color.BLACK);
+
+        Rectangle r = new Rectangle(25,25,250,250);
+        r.setFill(Color.BLUE);
+
+        root.getChildren().add(r);
+        primaryStage.setTitle("BlackJack");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
